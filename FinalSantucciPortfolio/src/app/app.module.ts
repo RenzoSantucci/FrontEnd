a8c900c68b4d28e8d000a083dd9e3b9aabd484b3
RenzoSantucci <renzosantucci2003@gmail.com>
2023-03-25 04:41:53 -0300

    Merge de redes sociales al header

--- FinalSantucciPortfolio/src/app/app.module.ts
+++ FinalSantucciPortfolio/src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './componentes/header/header.component';
 import { LogoYPComponent } from './componentes/logo-yp/logo-yp.component';
-import { RedesSocComponent } from './componentes/redes-soc/redes-soc.component';
 import { BannerComponent } from './componentes/banner/banner.component';
 import { AcercaDeComponent } from './componentes/acerca-de/acerca-de.component';
 import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
@@ -20,7 +19,6 @@ import { FooterComponent } from './componentes/footer/footer.component';
     AppComponent,
     HeaderComponent,
     LogoYPComponent,
-    RedesSocComponent,
     BannerComponent,
     AcercaDeComponent,
     ExperienciaComponent,
